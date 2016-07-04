7e29a37f01443c5da7dbc00e00a4e269b67902fb
BladeCode <Jerry.x@outlook.com>
2016-07-04 09:17:53 +0800

    add multi-channel

--- app/src/main/java/com/mhome/gradledeploy/MainActivity.java
+++ app/src/main/java/com/mhome/gradledeploy/MainActivity.java
@@ -2,12 +2,17 @@ package com.mhome.gradledeploy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String API_URL = BuildConfig.API_URL;
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        mTextView.setText(API_URL);
     }
 }
